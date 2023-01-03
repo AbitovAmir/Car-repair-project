1c39ffdc6568ede4560407d69857120bc4a30653
AbitovAmir <121855948+AbitovAmir@users.noreply.github.com>
2023-01-03 13:35:16 +0500

    Add files via upload

---
+++ Car repair/script.js
@@ -0,0 +1,11 @@
+let header = document.querySelector('.js-header'),
+    headerH = document.querySelector('.js-header').clientHeight;
+
+document.onscroll = function (){
+    let scroll = window.scrollY;
+    if (scroll > headerH){
+    header.classList.add('fixed')
+    }else {
+    header.classList.remove('fixed')
+    }
+}
